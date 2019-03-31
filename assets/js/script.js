c3b7186c33fc28ec335e088518b0c1e0e85a136c
meylisday <victory.sett@gmail.com>
2019-03-31 04:26:07 +0300

    fixed scrolling on tablet and mobile layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
-(function() {
-    var burger = document.querySelector('.burger');
-    var nav = document.querySelector('#'+burger.dataset.target);
-   
-    burger.addEventListener('click', function(){
-      burger.classList.toggle('is-active');
-      nav.classList.toggle('is-active');
-    });
-  })();
+(function () {
+  var burger = document.querySelector('.burger');
+  var nav = document.querySelector('#' + burger.dataset.target);
+
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('is-active');
+    nav.classList.toggle('is-active');
+    checkDevice(user);
+  });
+  let user = detect.parse(navigator.userAgent);
+  function checkDevice(user) {
+    let checkMenu = document.getElementById('navbarBurger');
+    let checkActiveMenu = checkMenu.classList.contains('is-active');
+    if (checkActiveMenu) {
+      if (user.device.type == 'Tablet' || user.device.type == 'Mobile') {
+        return document.documentElement.style.overflow = "hidden";
+      }
+    } else {
+      return document.documentElement.style.overflow = "auto";
+    }
+  }
+})();
